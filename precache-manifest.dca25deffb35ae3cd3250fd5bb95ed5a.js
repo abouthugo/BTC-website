6d2aaf8594f791de51f3dfb90a7b4cfee1483d74
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-04-08 23:29:06 -0400

    Updates

---
+++ precache-manifest.dca25deffb35ae3cd3250fd5bb95ed5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0df666d66632effd0c76",
+    "url": "/BTC-website/static/js/runtime~main.cbe76e4b.js"
+  },
+  {
+    "revision": "ec2508771e3b7dbe8ca8",
+    "url": "/BTC-website/static/js/main.d4d61428.chunk.js"
+  },
+  {
+    "revision": "148b69f03d9116b27f43",
+    "url": "/BTC-website/static/js/2.fdfdd0df.chunk.js"
+  },
+  {
+    "revision": "ec2508771e3b7dbe8ca8",
+    "url": "/BTC-website/static/css/main.6ab11b85.chunk.css"
+  },
+  {
+    "revision": "695ec09965d12fd12bef62a953e4cf87",
+    "url": "/BTC-website/index.html"
+  }
+];
